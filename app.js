406213307d86e4db8a2ea314df06f62c7ebb88b5
FredyHenao <jhongallego45y@hotmail.com>
2017-02-06 19:18:01 -0500

    Video 9
    tipos de datos primitivos y objetos.

---
+++ app.js
@@ -0,0 +1,20 @@
+var c = 1;
+var b = "fredy";
+//tipos de impresion por consola
+console.log(c);
+console.warn(b);
+console.error(c);
+console.info(b);
+//datos primitivos
+var num = 10;
+var str = "texto";
+var bool = true;
+var und = undefined;
+var nul = null;
+//objetos
+var obj ={
+  //propiedades
+  numero: 10,
+  texto: "nuevo texto"
+};
+console.log(obj);
