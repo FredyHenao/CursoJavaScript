5dd935c7615d655f686a12903113759a45fc13d9
FredyHenao <jhongallego45y@hotmail.com>
2017-02-06 20:52:43 -0500

    Video 13
    Parametros para las funciones

--- funciones.js
+++ funciones.js
@@ -1,10 +1,35 @@
 
-var a = 50;
+/*var a = 50;
 function primeraFuncion() {
   var a = 20;
   console.log(a);
 }
 
-primeraFuncion();
+primeraFuncion();*/
 
 //las funciones siempre devuelven un valor
+
+/*function imprimir(nombre, apellido) {
+    apellido = apellido || "xxx";
+    console.log(nombre+" "+apellido);
+}
+
+imprimir("fredy", "henao");*/
+
+//funciones con objetos
+/*function imprimir( persona ) {
+    console.log( persona.nombre+" "+persona.apellido);
+}
+imprimir({
+  nombre: "fredy",
+  apellido: "henao"
+});*/
+
+//enviar funciones a funciones
+function imprimir( fn ) {
+    fn();
+}
+
+imprimir(function () {
+  console.log("funcion anonima");
+});
