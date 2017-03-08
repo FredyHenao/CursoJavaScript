5a6dcb74fcb6e1e1baa95530bc80cb096cef4e8f
FredyHenao <jhongallego45y@hotmail.com>
2017-03-07 23:16:21 -0500

    Video 28
    Objeto Number.

---
+++ Obj_number.js
@@ -0,0 +1,9 @@
+
+var a = 10.78965;
+var b = new Number(10);
+//la funcion toFixed() sirve para poner decimales o cortarlos de un rango de 0 a 20 example a.toFixed(6)
+/*console.log(a);
+a = a.toString();
+console.log(a);*/
+//la funcion toPrecision() sirve para redondear el  numero  example a.toPrecision(4)
+//NaN(not a Number)
