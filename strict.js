6e7bdb44cc1584218b93075badf17e0d31f3294d
FredyHenao <jhongallego45y@hotmail.com>
2017-10-24 19:18:23 -0500

    Eventos

---
+++ strict.js
@@ -0,0 +1,9 @@
+/**
+ * Created by FredyHenao on 24/10/2017.
+ */
+//modo estricto
+
+"use strict"; //se encarga de decirle a javascript que sea mas estricto
+
+nombre = "Fredy";
+console.log(nombre);
